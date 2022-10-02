11b934b0bd366285d12a4dc16feff72aec73cd9d
Nicole Mah <mnmah@alaska.edu>
2022-10-02 15:54:23 -0800

    Added error.html to render when no entrie found

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -7,5 +7,9 @@ def index(request):
     return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
 
 def wiki(request,title):
-    return render(request, "encyclopedia/wiki.html",{"contents": util.get_entry(title), "title": title})
+    contents = util.get_entry(title)
+    if contents == None:
+        return render(request, "encyclopedia/error.html", {"title": title})
+    else:
+        return render(request, "encyclopedia/wiki.html",{"contents": contents, "title": title})
 
