2de69dc7836546626a9bf2fab2cebd07729dd26d
Nicole Mah <mnmah@alaska.edu>
2022-10-04 13:13:30 -0800

    FIXED SAVE AND EDIT BUG! replaced ":" with "="

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
     path("search/", views.search, name="search"),
-    path("newpage/", views.new_page, name="new_page"),
+    path("new/", views.new_page, name="new_page"),
     path("edit/", views.edit, name="edit"),
     path("save_edit/", views.save_edit, name="save_edit"),
-    path("rand/", views.rand, name="rand")
+    path("rand/", views.rand, name="rand"),
 
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -18,31 +18,29 @@ def convert_md_to_html(title):
         return markdowner.convert(content)
 
 def entry(request,title):
-    content = convert_md_to_html(title)
-    if content == None:
+    html_content = convert_md_to_html(title)
+    if html_content == None:
         return render(request, "encyclopedia/error.html", {"title": title, "message":"This page does not exist for "})
     else:
-        return render(request, "encyclopedia/entry.html",{"content": content, "title": title})
+        return render(request, "encyclopedia/entry.html",{"content": html_content, "title": title})
 
 def search(request):
     if request.method == "POST":
-        search_entry = request.POST['q']
-        content = convert_md_to_html(search_entry)
-        entries = util.list_entries()
-        for entry in entries:
-            if search_entry.lower() == entry.lower():
-                return render(request, "encyclopedia/entry.html",{"content": content, "title": search_entry})
+        entry_search = request.POST['q']
+        html_content = convert_md_to_html(entry_search)
+        if html_content is not None:
+            return render(request, "encyclopedia/entry.html",{"title":entry_search, "content": html_content})
         else:
-            search_results = []
-            for entry in entries:
-                if search_entry.lower() in entry.lower():
+            allEntries = util.list_entries()
+            search_results =[]
+            for entry in allEntries:
+                if entry_search.lower() in entry.lower():
                     search_results.append(entry)
-
-            return render(request, "encyclopedia/search.html", {"results": search_results})
+            return render(request, "encyclopedia/search.html",{"results": search_results})
 
 def new_page(request):
     if request.method == "GET":
-        return render(request, "encyclopedia/newpage.html")
+        return render(request, "encyclopedia/new.html")
     else:
         title = request.POST['title']
         content = request.POST['content']
