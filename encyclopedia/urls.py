2a214f6c064d6d7afe24806e90bab8248d5246d8
Nicole Mah <mnmah@alaska.edu>
2022-10-05 11:02:22 -0800

    Added rediect to error page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,11 +4,12 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry"),
+    path("wiki/<str:title>", views.page, name="page"),
     path("search/", views.search, name="search"),
     path("new/", views.new_page, name="new_page"),
     path("edit/", views.edit, name="edit"),
     path("save_edit/", views.save_edit, name="save_edit"),
-    path("rand/", views.rand, name="rand"),
+    path("random/", views.random_page, name="random_page"),
+    path("error", views.error, name="error")
 
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from . import util
 import markdown
 import random
+from django.contrib import messages
 
 
 def index(request):
@@ -9,7 +10,7 @@ def index(request):
     entries = util.list_entries()
     return render(request, "encyclopedia/index.html", {"entries": entries})
 
-def convert_md_to_html(title):
+def convert_to_html(title):
     content = util.get_entry(title)
     markdowner = markdown.Markdown()
     if content == None:
@@ -17,26 +18,27 @@ def convert_md_to_html(title):
     else:
         return markdowner.convert(content)
 
-def entry(request,title):
-    html_content = convert_md_to_html(title)
+def page(request,title):
+    html_content = convert_to_html(title)
     if html_content == None:
-        return render(request, "encyclopedia/error.html", {"title": title, "message":"This page does not exist for "})
+        messages.error(request, f"This page does not exist.")
+        return redirect("error")
     else:
-        return render(request, "encyclopedia/entry.html",{"content": html_content, "title": title})
+        return render(request, "encyclopedia/page.html",{"content": html_content, "title": title})
 
 def search(request):
     if request.method == "POST":
         entry_search = request.POST['q']
-        html_content = convert_md_to_html(entry_search)
+        html_content = convert_to_html(entry_search)
         if html_content is not None:
-            return render(request, "encyclopedia/entry.html",{"title":entry_search, "content": html_content})
+            return render(request, "encyclopedia/page.html",{"title":entry_search, "content": html_content})
         else:
-            allEntries = util.list_entries()
+            all_entries = util.list_entries()
             search_results =[]
-            for entry in allEntries:
+            for entry in all_entries:
                 if entry_search.lower() in entry.lower():
                     search_results.append(entry)
-            return render(request, "encyclopedia/search.html",{"results": search_results})
+            return render(request, "encyclopedia/search.html",{"results": search_results, "title":entry_search})
 
 def new_page(request):
     if request.method == "GET":
@@ -48,18 +50,18 @@ def new_page(request):
         title_exists = util.get_entry(title)
 
         if title_exists is not None:
-            return render(request, "encyclopedia/error.html", {"message": "Page with that title already exists"})
+            messages.error(request, f"Page with that title already exists.")
+            return redirect("error")
         else:
             util.save_entry(title,content)
-            html_content = convert_md_to_html(title)
-            return render(request, "encyclopedia/entry.html", {"title":title, "content": html_content})
+            html_content = convert_to_html(title)
+            return redirect("entry", {"title":title})
 
 def edit(request):
     if request.method == "POST":
         title = request.POST['curr_title']
         content = util.get_entry(title)
         return render(request, "encyclopedia/edit.html",{"title": title, "content":content})
-
 
     return(request)
 
@@ -68,11 +70,14 @@ def save_edit(request):
         title = request.POST['title']
         updated_content = request.POST['text']
         util.save_entry(title,updated_content)
-        html_content = convert_md_to_html(title)
-        return render(request, "encyclopedia/entry.html", {"title":title, "content": html_content})
+        html_content = convert_to_html(title)
+        return render(request, "encyclopedia/page.html", {"title":title, "content": html_content})
 
-def rand(request):
+def random_page(request):
     all_entries = util.list_entries()
     rand_entry = random.choice(all_entries)
-    html_content = convert_md_to_html(rand_entry)
-    return render(request, "encyclopedia/entry.html", {"title": rand_entry, "content": html_content})
+    html_content = convert_to_html(rand_entry)
+    return render(request, "encyclopedia/page.html", {"title": rand_entry, "content": html_content})
+
+def error(request):
+    return render(request, 'encyclopedia/error.html')
